4be42a63f141013e29417ac1c06f64e82787bae9
Isuru Siriwardana <ewisuru@gmail.com>
2021-03-08 10:19:11 +0800

    Update AwsSdkUsageDemoApplication.java
    Added create user, and update password methods

--- src/main/java/simple/AwsSdkUsageDemoApplication.java
+++ src/main/java/simple/AwsSdkUsageDemoApplication.java
@@ -3,14 +3,11 @@ package simple;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolsRequest;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolsResponse;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUsersRequest;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUsersResponse;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
 
 public class AwsSdkUsageDemoApplication {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
         ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.builder()
                 .profileName("cognito-admin")
                 .build();
@@ -20,14 +17,65 @@ public class AwsSdkUsageDemoApplication {
                 .credentialsProvider(credentialsProvider)
                 .build();
 
-        //listUserPools(cognitoClient);
+        String userPoolId = "ap-southeast-1_ksp0kj5aF";
 
-        ListUsersResponse listUsersResponse = cognitoClient.listUsers(ListUsersRequest.builder()
-                .userPoolId("ap-southeast-1_BVnzIKmk5")
-                .build());
-        listUsersResponse.users().forEach(user -> {
-            System.out.println(user);
-        });
+        //createAdmin(cognitoClient,userPoolId , "testuser2", "tsetuser2@gmail.com");
+        updatePassword(cognitoClient, userPoolId, "testuser2", "1q2w3e4r5t");
+
+
+    }
+
+    public static void updatePassword(CognitoIdentityProviderClient cognitoclient,
+                                   String userPoolId,
+                                   String name,
+                                   String newPassword){
+
+        try{
+            AdminSetUserPasswordResponse response = cognitoclient.adminSetUserPassword(
+                    AdminSetUserPasswordRequest
+                            .builder()
+                            .username(name)
+                            .password("1q2w3e4r5t")
+                            .userPoolId(userPoolId)
+                            .permanent(true)
+                            .build()
+            );
+
+            System.out.println("Changed password for user: " + name + "to: " + newPassword);
+
+        } catch (CognitoIdentityProviderException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
+
+    public static void createAdmin(CognitoIdentityProviderClient cognitoclient,
+                                   String userPoolId,
+                                   String name,
+                                   String email){
+
+        try{
+            AdminCreateUserResponse response = cognitoclient.adminCreateUser(
+                    AdminCreateUserRequest.builder()
+                            .userPoolId(userPoolId)
+                            .username(name)
+                            .userAttributes(AttributeType.builder()
+                                    .name("email")
+                                    .value(email)
+                                    .name("custom:id")
+                                    .value("G2342432423N")
+                                    .build())
+                            .messageAction("SUPPRESS")
+                            .build()
+            );
+
+            System.out.println("User " + response.user().username() + "is created. Status: " + response.user().userStatus());
+
+        } catch (CognitoIdentityProviderException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        //snippet-end:[cognito.java2.add_login_provider.main]
     }
 
     private static void listUserPools(CognitoIdentityProviderClient cognitoClient) {
